b4cffb547c76043ff00fe99c97944f183854aa42
Gustavo Lima <tavoks@uninove.edu.br>
2020-09-21 03:22:23 -0300

    Implementando Cors

--- src/Yugioh.Api/Startup.cs
+++ src/Yugioh.Api/Startup.cs
@@ -92,11 +92,12 @@ namespace Yugioh.Api
         {
             if (env.IsDevelopment())
             {
+                app.UseCors("Development");
                 app.UseDeveloperExceptionPage();
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseCors("Production");
                 app.UseHsts();
             }
 
